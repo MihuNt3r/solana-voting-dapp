c8da585bbdf77f6d1a794d053dc6eb5d5dcd663a
MihuNt3r <borovykmis@gmail.com>
2025-03-31 11:54:13 +0000

    Add helping comment

--- anchor/programs/solanavotingdapp/src/instructions/vote.rs
+++ anchor/programs/solanavotingdapp/src/instructions/vote.rs
@@ -1,7 +1,7 @@
 use anchor_lang::prelude::*;
 
-use crate::state::{poll_account::PollAccount, voter_account::VoterAccount};
 use crate::domain::ErrorCode;
+use crate::state::{poll_account::PollAccount, voter_account::VoterAccount};
 
 #[derive(Accounts)]
 #[instruction(poll_name: String, candidate: String)]
@@ -13,7 +13,7 @@ pub struct Vote<'info> {
         init_if_needed,
         payer = signer,
         space = 8 + VoterAccount::INIT_SPACE,
-        seeds = [b"voter".as_ref(), poll_name.as_ref(), signer.key().as_ref()],
+        seeds = [b"voter".as_ref(), poll_name.as_ref(), signer.key().as_ref()], // It is possible to generate only one PDA per voter and poll. In this way it is not possible to vote twice for the same poll
         bump
     )]
     pub voter_account: Account<'info, VoterAccount>,
@@ -29,28 +29,28 @@ pub struct Vote<'info> {
 }
 
 pub fn handle_vote(ctx: Context<Vote>, _poll_name: String, candidate: String) -> Result<()> {
-	let poll_account = &mut ctx.accounts.poll_account;
-	let signer = &mut ctx.accounts.signer;
+    let poll_account = &mut ctx.accounts.poll_account;
+    let signer = &mut ctx.accounts.signer;
 
-	// Check if the voter has already voted
-	let vote_account = &ctx.accounts.voter_account;
-	require!(vote_account.voter != *signer.key, ErrorCode::AlreadyVoted);
+    // Check if the voter has already voted
+    let vote_account = &ctx.accounts.voter_account;
+    require!(vote_account.voter != *signer.key, ErrorCode::AlreadyVoted);
 
-	// Find the candidate
-	let candidate = poll_account
-		.proposals
-		.iter_mut()
-		.find(|proposal| proposal.candidate_name() == candidate)
-		.ok_or(ErrorCode::CandidateNotFound)?;
+    // Find the candidate
+    let candidate = poll_account
+        .proposals
+        .iter_mut()
+        .find(|proposal| proposal.candidate_name() == candidate)
+        .ok_or(ErrorCode::CandidateNotFound)?;
 
-	// Increment the candidate's votes
-	candidate.vote();
+    // Increment the candidate's votes
+    candidate.vote();
 
-	// Store the vote in the PDA account
-	let vote_account = &mut ctx.accounts.voter_account;
+    // Store the vote in the PDA account
+    let vote_account = &mut ctx.accounts.voter_account;
 
-	vote_account.voter = *signer.key;
-	vote_account.voted_for = candidate.candidate_name().to_string();
+    vote_account.voter = *signer.key;
+    vote_account.voted_for = candidate.candidate_name().to_string();
 
-	Ok(())
+    Ok(())
 }
